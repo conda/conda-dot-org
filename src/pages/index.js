4c365be708ddfef3af4c6570975a0f091ebd40ea
Asmit2952 <asmitbm2952002@gmail.com>
2022-11-16 21:55:02 +0530

    feat: feature section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import condac from '@site/static/img/conda_c.png'
-import styles from './index.module.css';
+import Header from '../components/header';
+import Features from '../components/features';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="The place for everything regarding the conda packaging ecosystem">
       <main>
-      <div className={ [styles.header, styles.section_padding].join(' ') }>
-      <div className={ [styles.header_content, styles.header_content_image].join(' ') }>
-        <h1 className={styles.gradient_text}>Package, dependency and environment management for any language</h1>
-        <p>Conda is an open source package management system and environment management system that runs on Windows, macOS, Linux and z/OS. Conda quickly installs, runs and updates packages and their dependencies.</p>
-      
-        <div className={styles.header_content_input}>
-          <a href='/docs/intro'>Get Started</a>
-        </div>
-      
-      </div>
-      <div className={styles.header_image}>
-          <img src={condac} alt="conda logo" />
-      </div>
-      </div>
+        <Header />
+        <Features />
       </main>
     </Layout>
   );
