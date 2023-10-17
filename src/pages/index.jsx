93acb1fc578d0c87a82a74efe3e1476389042cf6
Ken Odegard <kodegard@anaconda.com>
2023-10-16 22:01:33 -0500

    Prettify

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import Header from '../components/Header';
-import Info from '../components/Info';
-import Features from '../components/Features';
-import News from '../components/News';
-import styles from './index.module.css';
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Header from "../components/Header";
+import Info from "../components/Info";
+import Features from "../components/Features";
+import News from "../components/News";
+import styles from "./index.module.css";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
