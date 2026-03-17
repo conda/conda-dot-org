32803dcb721a243c981382d077887c9fc5ffb2aa
jaimergp <jaimergp@users.noreply.github.com>
2026-03-17 14:19:02 +0100

    Only render attribution in full bllog post view

--- src/theme/BlogPostItem/Header/Banner/index.jsx
+++ src/theme/BlogPostItem/Header/Banner/index.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Image from "@theme/IdealImage";
 // starting 3.5.0 blog internals have moved to the plugin
 import { useBlogPost } from "@docusaurus/plugin-content-blog/client";
 
 export default function BlogPostItemHeaderBanner() {
-  const { metadata } = useBlogPost();
+  const { metadata, isBlogPostPage } = useBlogPost();
   const { frontMatter } = metadata;
   const { image, image_credit, title } = frontMatter;
 
@@ -27,7 +26,7 @@ export default function BlogPostItemHeaderBanner() {
           alt={`Banner image for ${title} blog post`}
         />
       )}
-      {image && image_credit && <ImageCredit image_credit={image_credit} />}
+      {isBlogPostPage && image && image_credit && <ImageCredit image_credit={image_credit} />}
     </div>
   );
 }
